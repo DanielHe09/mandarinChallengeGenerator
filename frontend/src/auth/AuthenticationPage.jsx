823dc07157fe40d43baadc221ce39682a80e59c5
DanielHe09 <dandanhe09@gmail.com>
2025-07-01 18:43:13 -0400

    added Clerk authentication

--- frontend/src/auth/AuthenticationPage.jsx
+++ frontend/src/auth/AuthenticationPage.jsx
@@ -4,10 +4,13 @@ import {SignIn, SignUp, SignedIn, SignedOut} from "@clerk/clerk-react"
 export function AuthenticationPage() {
     return <div className="auth-container">
 
+        {/* renders only if Clerk checks the user is not signed in */}
         <SignedOut>
             <SignIn routing="path" path="/sign-in" />
             <SignUp routing="path" path="/sign-up" />
         </SignedOut>
+
+        {/* renders only if Clerk checks the user is signed in */}
         <SignedIn>
             <div className="redirect-message">
                 <p>You are already signed in!</p>
