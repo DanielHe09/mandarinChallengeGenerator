729f9873743e2173dbb87c5008f5a922c399eedc
DanielHe09 <dandanhe09@gmail.com>
2025-07-27 23:07:14 -0400

    layed basic layout for ChallengeGenerator component

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -1,5 +1,23 @@
 import "react"
+import { useState, useEffect } from "react"
+import {MCQChallenge} from "./MCQChallenge.jsx"
 
 export function ChallengeGenerator() {
-    return <></>
+    const [challenge, setChallenge] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [difficulty, setDifficulty] = useState("easy")
+    const[quota, setQuota] = useState(null)
+
+    {/*function for seeing how many challenge generation tokens the user has left*/}
+    const fetchQuota = async () => {}
+
+    const generateChallenge = async() =>{}
+
+    {/*determine how long till the user gets more credits (every 24h the user gets more credits) */}
+    const getnextResetTime = () => {}
+
+    return <div className = "challenge-container">
+        <h2>Mandarin Challenge Generator</h2>
+    </div>
 }
