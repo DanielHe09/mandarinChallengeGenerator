895b12e76c5aba7b4e5217004c8d1b6916d0102b
DanielHe09 <dandanhe09@gmail.com>
2025-07-29 23:19:27 -0400

    added imports and useState variables for HistoryPanel component

--- frontend/src/history/HistoryPanel.jsx
+++ frontend/src/history/HistoryPanel.jsx
@@ -1,5 +1,19 @@
 import "react"
+import {useState, useEffect} from "react"
+import { MCQChallenge } from "../challenge/MCQChallenge"
 
 export function HistoryPanel() {
+    const[history, setHistory] = useState([])
+    const [isloading, setIsLoading] = useState(true)
+    const[error, setError] = useState(null)
+
+    useEffect(() =>{
+        fetchHistory()
+    }, [])
+
+    const fetchHistory = async () => {
+        setIsLoading(false)
+    }
+    
     return <></>
 }
