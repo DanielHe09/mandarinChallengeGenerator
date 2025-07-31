4b8ffab7dc3617377436515639a889903f5c2932
DanielHe09 <dandanhe09@gmail.com>
2025-07-30 23:24:59 -0400

    created a fastapi server

---
+++ backend/src/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from clerk_backend_api import Clerk 
+import os
+
+#reading the clerk secret key from .env to create a clerk SDK instance for authentication
+clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+
+#creating a new FastAPI web server
+app = FastAPI()
+
+#CORS=cross-origin resource sharing, allows frontend to talk to backend (they're different servers)
+#allows requests from anywhere, any cookies/auth tokens, all HTTP methods, all request headers
+app.add_middlware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=[*], allow_headers=[*])
