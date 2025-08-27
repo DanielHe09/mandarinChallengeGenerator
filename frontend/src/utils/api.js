7ed2019d410a7ee1cd11c44c20b5761d36d0527b
DanielHe09 <dandanhe09@gmail.com>
2025-08-27 13:27:43 -0400

    finished custom useApi react hook to make request to backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-//function that allows us to send a request with the clerk token to our backend
+//custom-made react hook that allows us to send a request with the clerk token to our backend
 
 import {useAuth} from "@clerk/clerk-react"
 
@@ -15,6 +15,7 @@ export const useApi = () => {
             }
         }
 
+        //get make request and get request from backend
         const response = await fetch('http://localhost:8000/api/${endpoint}', {
             /*
             the three dots is the spread operators, we combine the two objects by spreading their properties.
@@ -23,6 +24,21 @@ export const useApi = () => {
             ...defaultOptions,
             ...options,
         })
+
+        if (!response.ok) {
+            //if parsing of response json fails, catches error and returns null
+            const errorData = await response.json().catch(() => null)
+            if (response.status === 429) {
+                throw new Error("Daily quota exceeded")
+            }
+            throw new Error(errorData?.detail || "An error occurred")
+        }
+
+        return response.json()
+
     }
+
+    //making our useApi() hook flexible for future, this makes the makeRequest function available in other 
+    return {makeRequest}
 
 }
