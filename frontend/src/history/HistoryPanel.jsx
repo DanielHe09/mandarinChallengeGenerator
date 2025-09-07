7aa4f054d2257391ce16cf5076a72e48fbe60633
DanielHe09 <dandanhe09@gmail.com>
2025-09-06 23:43:33 -0400

    got history panel to work

--- frontend/src/history/HistoryPanel.jsx
+++ frontend/src/history/HistoryPanel.jsx
@@ -1,8 +1,10 @@
 import "react"
 import {useState, useEffect} from "react"
 import { MCQChallenge } from "../challenge/MCQChallenge"
+import {useApi} from "../utils/api"
 
 export function HistoryPanel() {
+    const {makeRequest} = useApi()
     const[history, setHistory] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const[error, setError] = useState(null)
@@ -14,7 +16,18 @@ export function HistoryPanel() {
     }, [])
 
     const fetchHistory = async () => {
-        setIsLoading(false)
+        setIsLoading(true)
+        setError(null)
+
+        try {
+            const data = await makeRequest("my-history")
+            setHistory(data.challenges)
+        } catch (err) {
+            setError(err.message || "Failed to fetch history.")
+        } finally {
+            setIsLoading(false)
+        }
+
     }
 
     {/*preparing different return statements based on the status of our page */}
