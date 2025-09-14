006ee53e1efdc0fcccf6437c7ba5ae109c58380c
DanielHe09 <dandanhe09@gmail.com>
2025-09-13 22:59:14 -0400

    fixed formatting

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,7 +8,13 @@ app = FastAPI()
 
 #CORS=cross-origin resource sharing, allows frontend to talk to backend (they're different servers)
 #allows requests from anywhere, any cookies/auth tokens, all HTTP methods, all request headers
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
 
 #"add this collection of routes to the main app"
 app.include_router(challenge.router, prefix="/api")
